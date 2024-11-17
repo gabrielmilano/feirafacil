92434f5b4746c7f6bfd87f3dd3489ce5b7aa01d0
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2024-11-17 12:40:14 -0300

    att

--- app/pages/aut/api/user/apiService.ts
+++ app/pages/aut/api/user/apiService.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api'; 
+const API_URL = process.env.REACT_APP_API_URL || 'https://feira.mateuslh.com/api/feiras'; 
 
 const getToken = () => {
     const name = 'token=';
