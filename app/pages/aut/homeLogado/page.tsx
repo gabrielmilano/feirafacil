cf461cf43b1676cfb1bea8f3dc454f638a1a942f
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-11-26 12:42:08 -0300

    Update page.tsx

--- app/pages/aut/homeLogado/page.tsx
+++ app/pages/aut/homeLogado/page.tsx
@@ -5,13 +5,14 @@ import withAuth from '../../../hoc/withAuth';
 
 function Home() {
   return (
-      <div className="flex flex-col min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <header className="bg-[#f3f4f6] text-black py-4 text-center">
         <h1 className="text-4xl font-bold">Portal do Administrador</h1>
         <p className="mt-2">Bem-vindo de volta!</p>
       </header>
-      <main className="flex-grow flex flex-col items-center justify-center p-6 bg-gray-100">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+      <main className="flex-grow flex flex-col items-center p-6 bg-gray-100">
+        {/* Ajuste para os cards ficarem mais acima */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mb-8">
           {/* Card de Feirantes */}
           <Link href="/pages/aut/feirantes">
             <div className="bg-white shadow-lg rounded-lg overflow-hidden hover:scale-105 transform transition-all duration-300">
@@ -47,6 +48,25 @@ function Home() {
               </div>
             </div>
           </Link>
+        </div>
+
+        {/* Seção abaixo dos cards */}
+        <div className="text-center mb-12 px-4">
+          <p className="text-lg text-gray-700 mb-4">
+            Aqui você pode gerenciar feirantes, feiras e eventos de forma rápida e eficiente. Acesse as seções acima para realizar todas as suas atividades administrativas, como adicionar feirantes, criar novas feiras e editar os eventos das respectivas feiras.
+          </p>
+          <div className="flex justify-center">
+            {/* Espaço para o vídeo do YouTube */}
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/watch?v=arxlQTqLURg"  // Substitua pelo link do vídeo
+              title="Vídeo explicativo"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </main>
     </div>
