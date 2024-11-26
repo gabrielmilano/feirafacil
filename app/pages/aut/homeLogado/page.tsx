a47a410ed166245729d71a2a3b223cecd5643146
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-11-26 12:26:29 -0300

    Update page.tsx

--- app/pages/aut/homeLogado/page.tsx
+++ app/pages/aut/homeLogado/page.tsx
@@ -5,8 +5,8 @@ import withAuth from '../../../hoc/withAuth';
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
-      <header className="bg-[#d1d1d1] text-white py-4 text-center">
+      <div className="flex flex-col min-h-screen bg-gray-50">
+      <header className="text-white py-4 text-center">
         <h1 className="text-4xl font-bold">Portal do Administrador</h1>
         <p className="mt-2">Bem-vindo de volta!</p>
       </header>
