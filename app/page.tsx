8cad6ddeec1d9d51cdd93d51450ce34c52b6c076
pauloFrancisconi <pauloffrancisconi@gmail.com>
2024-11-27 16:44:52 -0300

    [Ajuste] - preparando api.ts para receber pagina de feira publica

--- app/page.tsx
+++ app/page.tsx
@@ -109,25 +109,22 @@ export default function Home() {
                         <section className="mb-8">
                             <h2 className="text-2xl font-semibold mb-4">Próximas Feiras</h2>
                             {feiras.map((feira, index) => (
-                                <Link href={`/feiras/${feira.id}`} key={feira.id}>
-                                    <div className="mb-6 bg-white p-4 rounded shadow hover:cursor-pointer hover:bg-gray-100 transition">
-                                        <h3 className="text-xl font-bold text-gray-700">{feira.nome}</h3>
-                                        <p className="text-gray-700">{feira.descricao}</p>
-                                        <h4 className="text-lg font-semibold mt-4">Próximos Eventos:</h4>
-                                        {eventos[index]?.length ? (
-                                            eventos[index]
-                                                .filter(evento => new Date(evento.data) >= new Date())
-                                                .map(evento => (
-                                                    <p key={evento.id} className="text-gray-600">{new Date(evento.data).toLocaleDateString()}</p>
-                                                ))
-                                        ) : (
-                                            <p className="text-gray-500">Nenhum evento programado.</p>
-                                        )}
-                                    </div>
-                                </Link>
+                                <div key={feira.id} className="mb-6 bg-white p-4 rounded shadow">
+                                    <h3 className="text-xl font-bold text-gray-700">{feira.nome}</h3>
+                                    <p className="text-gray-700">{feira.descricao}</p>
+                                    <h4 className="text-lg font-semibold mt-4">Próximos Eventos:</h4>
+                                    {eventos[index]?.length ? (
+                                        eventos[index]
+                                            .filter(evento => new Date(evento.data) >= new Date()) // Filtra eventos passados
+                                            .map(evento => (
+                                                <p key={evento.id} className="text-gray-600">{new Date(evento.data).toLocaleDateString()}</p>
+                                            ))
+                                    ) : (
+                                        <p className="text-gray-500">Nenhum evento programado.</p>
+                                    )}
+                                </div>
                             ))}
                         </section>
-
                     )}
 
                     <section className="bg-gray-200 p-6 rounded-lg shadow-md">
