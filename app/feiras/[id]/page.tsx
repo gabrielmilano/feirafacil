66d0ba1dc36b28c87219a1ca5c3409db7c56f95c
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-11-29 13:57:27 -0300

    Update page.tsx

--- app/feiras/[id]/page.tsx
+++ app/feiras/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { fetchPublicFeiras } from '../../api/user/api';
 import { Feira } from '../../api/user/api';
 
